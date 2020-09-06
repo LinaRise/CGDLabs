6ee17be2f7638305b85c4cd840978826ebb64335
Lina <lilu3050@gmail.com>
2020-09-06 18:42:55 +0300

    final

--- src/sample/lab1/Main.java
+++ src/sample/lab1/Main.java
@@ -7,132 +7,146 @@ import javafx.concurrent.WorkerStateEvent;
 import javafx.event.EventHandler;
 import javafx.scene.Group;
 import javafx.scene.Scene;
-import javafx.scene.image.*;
+import javafx.scene.image.ImageView;
+import javafx.scene.image.PixelReader;
+import javafx.scene.image.PixelWriter;
+import javafx.scene.image.WritableImage;
 import javafx.scene.input.MouseEvent;
 import javafx.scene.paint.Color;
 import javafx.scene.shape.Polygon;
 import javafx.stage.Stage;
 
-
 import java.awt.geom.Point2D;
 import java.util.List;
 
 public class Main extends Application {
-  final int stageWidth = 600;
-  final int stageHeight = 600;
+  static final int stageWidth = 600;
+  static final int stageHeight = 600;
 
   @Override
-  public void start(Stage primaryStage) throws Exception {
+  public void start(Stage primaryStage) {
 
-    List<Point2D> listOfPoints = RandomPolygon.generate_polygon();
+    //генерация точек многоугольника
+    List<Point2D> listOfPoints = PolygonRandom.generatePolygon();
     Polygon polygon = new Polygon();
-    for (int i = 0; i < listOfPoints.size(); i++) {
-      polygon.getPoints().addAll(listOfPoints.get(i).getX(), listOfPoints.get(i).getY());
+    //добавляем точки
+    for (Point2D listOfPoint : listOfPoints) {
+      polygon.getPoints().addAll(listOfPoint.getX(), listOfPoint.getY());
     }
 
+    for (Point2D listOfPoint : listOfPoints) {
+      System.out.println(listOfPoint);
+    }
     polygon.setStroke(Color.BLACK);
     polygon.setFill(Color.TRANSPARENT);
 
     Group root = new Group();
 
+
     WritableImage writableImage = new WritableImage(stageWidth, stageHeight);
     ImageView imageView = new ImageView(writableImage);
-
     imageView.setFitHeight(stageHeight);
     imageView.setFitWidth(stageWidth);
-
     PixelWriter pixelWriter = writableImage.getPixelWriter();
-
-    PixelReader pixelReader = writableImage.getPixelReader();
-    System.out.println(pixelReader.getColor(20, 20));
-    pixelWriter.setColor(20, 20, Color.BLACK);
-    System.out.println(pixelReader.getColor(20, 20));
 
 
     root.getChildren().addAll(imageView, polygon);
-
     Scene scene = new Scene(root, stageWidth, stageHeight);
 
     boolean[][] pixelMatrix = new boolean[600][600];
 
+
+    scene.setOnMousePressed(event -> System.out.println(event.getX() + ";" + event.getY() + "!!!!"));
+
+    //при клике начнется выполнятся алгоритм, который при клике начнет закращивать с одной из сторон
     scene.setOnMouseClicked(new EventHandler<MouseEvent>() {
+      int pointNumber = -1;
+
       @Override
       public void handle(MouseEvent event) {
-        for (int pointNumber = 0; pointNumber < listOfPoints.size(); pointNumber++) {
-          if (pointNumber != listOfPoints.size() - 1) {
-            double minY = listOfPoints.get(pointNumber + 1).getY();
-            double maxY = listOfPoints.get(pointNumber).getY();
-            if (listOfPoints.get(pointNumber).getY() < listOfPoints.get(pointNumber + 1).getY()) {
-              minY = listOfPoints.get(pointNumber).getY();
-              maxY = listOfPoints.get(pointNumber + 1).getY();
-            }
+//        for (int pointNumber = 0; pointNumber < listOfPoints.size(); pointNumber++) {
 
-            for (int y = (int) minY; y < maxY; y++) {
-//              double currentStartX = (listOfPoints.get(pointNumber).getX() < listOfPoints.get(pointNumber + 1).getX())
-//                      ? listOfPoints.get(pointNumber).getX()
-//                      : listOfPoints.get(pointNumber + 1).getX();
+        pointNumber++;
+        if (pointNumber < listOfPoints.size() - 1) {
+          double minY = listOfPoints.get(pointNumber + 1).getY();
+          double maxY = listOfPoints.get(pointNumber).getY();
+          if (listOfPoints.get(pointNumber).getY() < listOfPoints.get(pointNumber + 1).getY()) {
+            minY = listOfPoints.get(pointNumber).getY();
+            maxY = listOfPoints.get(pointNumber + 1).getY();
+          }
 
-              double k = (listOfPoints.get(pointNumber).getY() - listOfPoints.get(pointNumber + 1).getY()) /
-                      (listOfPoints.get(pointNumber).getX() - listOfPoints.get(pointNumber + 1).getX());
-              double b = listOfPoints.get(pointNumber + 1).getY() -
-                      (listOfPoints.get(pointNumber).getY() - listOfPoints.get(pointNumber + 1).getY()) /
-                              (listOfPoints.get(pointNumber).getX() - listOfPoints.get(pointNumber + 1).getX()) *
-                              listOfPoints.get(pointNumber + 1).getX();
+          for (int y = (int) minY; y < maxY; y++) {
+//
 
-              double currentX = (y - b) / k;
-//              double divider1 = listOfPoints.get(pointNumber).getX() - listOfPoints.get(pointNumber + 1).getX();
-//              double divider2 = listOfPoints.get(pointNumber).getY() - listOfPoints.get(pointNumber + 1).getY();
-//              double divided2 = y - listOfPoints.get(pointNumber).getY();
-//             double currentStartX = listOfPoints.get(pointNumber).getX()
+            double k = (listOfPoints.get(pointNumber).getY() - listOfPoints.get(pointNumber + 1).getY()) /
+                    (listOfPoints.get(pointNumber).getX() - listOfPoints.get(pointNumber + 1).getX());
 
-              for (int x = (int) currentX; x < 600; x++) {
-                if (!pixelMatrix[x][y]) {
-                  pixelWriter.setColor(x, y, Color.GREY);
-                  pixelMatrix[x][y] = true;
-                } else {
-                  pixelWriter.setColor(x, y, Color.WHITE);
-                  pixelMatrix[x][y] = false;
-                }
-              }
-            }
-          } else {
+//              System.out.println(listOfPoints.get(pointNumber).getX() + ";" + listOfPoints.get(pointNumber).getY());
+            System.out.println(listOfPoints.get(pointNumber + 1).getX() + ";" + listOfPoints.get(pointNumber + 1).getY());
+//              Circle circle1 = new Circle(listOfPoints.get(pointNumber).getX(), listOfPoints.get(pointNumber).getY(), 5);
+//              Circle circle2 = new Circle(listOfPoints.get(pointNumber).getX(), listOfPoints.get(pointNumber).getY(), 5);
+//              root.getChildren().addAll(circle1, circle2);
 
-            double minY = listOfPoints.get(0).getY();
-            double maxY = listOfPoints.get(pointNumber).getY();
-            if (listOfPoints.get(pointNumber).getY() < listOfPoints.get(0).getY()) {
-              minY = listOfPoints.get(pointNumber).getY();
-              maxY = listOfPoints.get(0).getY();
-            }
 
-            for (int y = (int) minY; y < maxY; y++) {
-              double k = (listOfPoints.get(pointNumber).getY() - listOfPoints.get(0).getY()) /
-                      (listOfPoints.get(pointNumber).getX() - listOfPoints.get(0).getX());
-              double b = listOfPoints.get(0).getY() -
-                      (listOfPoints.get(pointNumber).getY() - listOfPoints.get(0).getY()) /
-                              (listOfPoints.get(pointNumber).getX() - listOfPoints.get(0).getX()) *
-                              listOfPoints.get(0).getX();
+            double b = listOfPoints.get(pointNumber + 1).getY() -
+                    ((listOfPoints.get(pointNumber).getY() - listOfPoints.get(pointNumber + 1).getY()) /
+                            (listOfPoints.get(pointNumber).getX() - listOfPoints.get(pointNumber + 1).getX())) *
+                            listOfPoints.get(pointNumber + 1).getX();
+            if (Double.isInfinite(k)) k = 0;
+//              System.out.println("k = " + k);
+            if (Double.isInfinite(b)) b = 0;
+//              System.out.println("b = " + b);
+            double currentX = (y - b) / k;
+            if (Double.isInfinite(currentX)) currentX = listOfPoints.get(pointNumber).getX();
+            System.out.println(currentX);
 
-              double currentX = (y - b) / k;
-              for (int x = (int) currentX; x < 600; x++) {
-                if (!pixelMatrix[x][y]) {
-                  pixelWriter.setColor(x, y, Color.GREY);
-                  pixelMatrix[x][y] = true;
-                } else {
-                  pixelWriter.setColor(x, y, Color.WHITE);
-                  pixelMatrix[x][y] = false;
-                }
+            for (int x = (int) currentX; x < 600; x++) {
+              if (!pixelMatrix[x][y]) {
+                pixelWriter.setColor(x, y, Color.GREY);
+                pixelMatrix[x][y] = true;
+              } else {
+                pixelWriter.setColor(x, y, Color.WHITE);
+                pixelMatrix[x][y] = false;
               }
             }
           }
+        } else if (pointNumber == listOfPoints.size() - 1){
 
+          double minY = listOfPoints.get(0).getY();
+          double maxY = listOfPoints.get(pointNumber).getY();
+          if (listOfPoints.get(pointNumber).getY() < listOfPoints.get(0).getY()) {
+            minY = listOfPoints.get(pointNumber).getY();
+            maxY = listOfPoints.get(0).getY();
+          }
+
+          for (int y = (int) minY; y < maxY; y++) {
+            double k = (listOfPoints.get(pointNumber).getY() - listOfPoints.get(0).getY()) /
+                    (listOfPoints.get(pointNumber).getX() - listOfPoints.get(0).getX());
+            double b = listOfPoints.get(0).getY() -
+                    (listOfPoints.get(pointNumber).getY() - listOfPoints.get(0).getY()) /
+                            (listOfPoints.get(pointNumber).getX() - listOfPoints.get(0).getX()) *
+                            listOfPoints.get(0).getX();
+
+            double currentX = (y - b) / k;
+            for (int x = (int) currentX; x < 600; x++) {
+              if (!pixelMatrix[x][y]) {
+                pixelWriter.setColor(x, y, Color.GREY);
+                pixelMatrix[x][y] = true;
+              } else {
+                pixelWriter.setColor(x, y, Color.WHITE);
+                pixelMatrix[x][y] = false;
+              }
+            }
+          }
         }
       }
-    });
+
+  });
 
 
     primaryStage.setScene(scene);
     primaryStage.show();
-  }
+}
 
 
   public static void main(String[] args) {
@@ -140,27 +154,4 @@ public class Main extends Application {
   }
 
 
-  private static class SleepService extends Service {
-    private static final int SLEEP_TIME = 2000;
-
-    private SleepService() {
-      setOnSucceeded(new EventHandler<WorkerStateEvent>() {
-        @Override
-        public void handle(WorkerStateEvent event) {
-          System.out.println("Freeze");
-        }
-      });
-    }
-
-    @Override
-    protected Task createTask() {
-      return new Task() {
-        @Override
-        protected String call() throws Exception {
-          Thread.sleep(SLEEP_TIME);
-          return "Freeze";
-        }
-      };
-    }
-  }
 }
