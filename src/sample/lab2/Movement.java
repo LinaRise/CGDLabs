b52e1c13ec883641d78e35de80c4cddda5e4fe6b
Lina <lilu3050@gmail.com>
2020-09-17 14:56:50 +0300

    лаб2-

--- src/sample/lab2/Movement.java
+++ src/sample/lab2/Movement.java
@@ -6,6 +6,7 @@ import java.util.List;
 public class Movement {
 
   static List<Point2D> moveFigure(List<Point2D> point2DList, int x, int y) {
+    //- y тк в программе начало координат идет в левом верхнем углу
     for (int i = 0; i < point2DList.size(); i++) {
       point2DList.set(i, new Point2D.Double(point2DList.get(i).getX() + x, point2DList.get(i).getY() - y));
     }
@@ -13,6 +14,7 @@ public class Movement {
   }
 
 
+  // поворот вокруш начала координат - находится в точке [300;300] - рассчитываем при помощи деления координатной плоскости на 2
   static List<Point2D> rotateFigure(List<Point2D> point2DList, int angle) {
     for (int i = 0; i < point2DList.size(); i++) {
       double newX = Test2.coordWidth/2 + (point2DList.get(i).getX() - Test2.coordWidth/2) * Math.cos(Math.toRadians(angle)) - (point2DList.get(i).getY() - Test2.coordHeight/2) * Math.sin(Math.toRadians(angle));
